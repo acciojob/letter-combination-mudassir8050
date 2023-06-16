a2e98a3f7d49fa1acafe975bb49137c36d71d104
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-16 13:37:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 function letterCombinations(input) {
-  //Complete the function
-	let table ={
-		"2" : "abc",
-		"3" : "def",
-		"4": "ghi",
-		"5": "jkl",
-		"6": "mno",
-		"7":"pqrs",
-		"8":"tuv",
-		"9":"wxyz"
-		
-	}
-	let solution= [];
-	function solve(currentIndex, calculatedString) {
-		if(input.length <= currentIndex){
-			solutions.push(calculatedString);
-			return;
-		}
-		let currentChar=input[currentIndex]
-		for(let i=0; i<table[currentChar]).length; i++){
-		solve(currentIndex + 1, calculatedString + table[currentChar][i])
-		
-	}
+ // let input = "368" ;
+// "3 6 8" 
+//        i
+
+// "d" => "dm" => "dmt"
+// "e" => "em" => "emu"
+// "d" => ["dm", "dn", "do"]
+// "e" => ["em" , "en", "eo"]
+// "f" => ["fm", "fn", "fo"]
+let table = {
+    "2" : "abc",
+    "3" : "def",
+    "4" : "ghi",
+    "5" : "jkl",
+    "6" : "mno",
+    "7" : "pqrs",
+    "8" : "tuv",
+    "9" : "wxyz"
+}
+
+let solutions = [] ;
+
+function solve(currentIndex, calculatedString) {
+    if(input.length <= currentIndex){
+        solutions.push(calculatedString);
+        return;
+    }
+    // currentIndex = 2, calculatedString = "dm"
+    let currentChar = input[currentIndex] // "3"
+    // table["3"] = "def"
+    for(let i = 0 ; i < table[currentChar].length; i++) {
+        // "def" 
+        solve(currentIndex + 1, calculatedString + table[currentChar][i] )
+        // solve(0 + 1, "" + "d")
+    }
 }
 
 solve(0, "")
+
 return solutions;
+
 }
 
 
