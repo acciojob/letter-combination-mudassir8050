bc42fde41a9b4f130024d9233958c9ccd1e2a3e7
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-16 13:08:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function letterCombinations(input_digit) {
+function letterCombinations(input) {
   //Complete the function
+	let table ={
+		"2" : "abc",
+		"3" : "def",
+		"4": "ghi",
+		"5": "jkl",
+		"6": "mno",
+		"7":"prqs",
+		"8":"tuv",
+		"9":"wxyz"
+		
+	}
+	let solution=[];
+	function solve(currentIndex, calculateString) {
+		if(input.length <=currentIndex){
+			solution.push(calculateString);
+			return;
+		}
+		let currentIndex=input[currentIndex]
+		for(let i=0; i<table[currentChar]).length;
+		solve(currentIndex+1, calculateString+table[currentChar])
+		
+	}
 }
+
+solve(0, "")
+return solutions;
+
 
 module.exports = letterCombinations;
